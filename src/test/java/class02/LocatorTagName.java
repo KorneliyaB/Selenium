a6addc3a62c2e293803d38de455cb9b8f6e6bf48
KorneliyaB <korneliyabanyas@gmail.com>
2023-05-23 13:39:50 -0500

    commit

--- src/test/java/class02/LocatorTagName.java
+++ src/test/java/class02/LocatorTagName.java
@@ -8,20 +8,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
 
 
-public class LocatorTagName {
+public class  LocatorTagName {
     public static void main(String[] args) {
         // navigate to amazon
         WebDriver driver=new ChromeDriver();
         driver.get("http:/www.amazon.com/");
         driver.manage().window().maximize();
 
-        //get all the links from amazon.com and print thm on the console
-        List<WebElement> allLinks=driver.findElements(By.tagName("a"));
+        //get all the links from amazon.com and print them on the console
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+
 
         //traverse
-        for(WebElement link:allLinks){
+        for (WebElement link:allLinks){
             String linkOfWebsite=link.getAttribute("href");
             System.out.println(linkOfWebsite);
+
+
         }
 
     }
