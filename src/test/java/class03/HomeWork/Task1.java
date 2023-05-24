b51052d95dc36d7857945c13937c93c2459e1cd3
KorneliyaB <korneliyabanyas@gmail.com>
2023-05-24 13:59:39 -0500

    commit

---
+++ src/test/java/class03/HomeWork/Task1.java
@@ -0,0 +1,10 @@
+package class03.HomeWork;
+
+public class Task1 {
+    /*
+    Open chrome browser
+Go to “http://practice.syntaxtechs.net/input-form-demo.php%E2%80%9D
+fill in the complete form
+
+     */
+}
---
+++ src/test/java/class03/HomeWork/Task2.java
@@ -0,0 +1,12 @@
+package class03.HomeWork;
+
+public class Task2 {
+    /*
+    Go to “http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login%E2%80%9D
+Enter valid username
+Leave password field empty
+Click on login button
+Verify error message with text “Password cannot be empty” is displayed.
+
+     */
+}
