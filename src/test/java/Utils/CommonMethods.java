d32769df4360925d203e12f22d8a82a238bd9637
KorneliyaB <korneliyabanyas@gmail.com>
2023-06-13 11:34:12 -0500

    commit

--- src/test/java/Utils/CommonMethods.java
+++ src/test/java/Utils/CommonMethods.java
@@ -1,23 +1,28 @@
 package Utils;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class CommonMethods {
     public static WebDriver driver;
 
-    public static void openBrowserAndLaunchApp(String URL, String browser){
-        switch (browser){
-            case"chrome":
-                driver=new ChromeDriver();
+    public static void openBrowserAndLaunchApp(String URL, String browser) {
+        switch (browser) {
+            case "chrome":
+                driver = new ChromeDriver();
                 break;
             case "firefox":
-                driver=new FirefoxDriver();
+                driver = new FirefoxDriver();
                 break;
         }
 //       maximize the window
@@ -27,14 +32,26 @@ public class CommonMethods {
         //implicit Wait
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
-    public static void closeBrowser(){
-        if (driver!=null){
+
+    public static void closeBrowser() {
+        if (driver != null) {
             driver.quit();
         }
     }
 
-    public static void sendText(String text, WebElement element){
+    public static void sendText(String text, WebElement element) {
         element.clear();
         element.sendKeys(text);
+    }
+
+    public static void screenShot(String fileName) {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File screenShot = ts.getScreenshotAs(OutputType.FILE);
+        String path = "C:\\Users\\korne\\IdeaProjects\\syntexSelB16\\screenshot\\" + fileName + ".png";
+        try {
+            FileUtils.copyFile(screenShot, new File(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
